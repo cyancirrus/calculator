56741ac07ff6b1fe72154e7c6d8ad7b2e67297ed
Raven Echo <ravenecho@Ravens-MacBook-Pro.local>
2021-03-30 12:53:33 -0500

    expirmenting with formatting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Computer from './computer'
+import ContainerExampleContainer from './visual'
+import ImageExampleImage from './images'
 
 function App() {
   return (
     <div>
+      <ImageExampleImage />
+      <ContainerExampleContainer />
       <Computer />
     </div>
   );
---
+++ src/images.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const ImageExampleImage = () => (
+  <Image src='./assets/images/astolfo.jpg' size='small' />
+)
+
+export default ImageExampleImage
