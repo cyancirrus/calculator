26a5a56c8bfbfd05e80a8421132c99b750dd538b
Raven Echo <ravenecho@Ravens-MacBook-Pro.local>
2021-04-05 22:01:10 -0500

    terrible fonting

--- src/ws_computation/body.js
+++ src/ws_computation/body.js
@@ -4,12 +4,13 @@ import { Container } from 'semantic-ui-react'
 const WebsiteBody = () => (
   <Container>
   <section>
-    <h1>
-      welcome to the current landing page for my javascript projects
-    </h1>
     <h2>
-      below is a calculator i made. more to come i promise 
+      welcome to the current landing page for my javascript projects
     </h2>
+    <h4>
+      below is a calculator i made. more to come i promise 
+    </h4>
+    <div class="ui hidden divider" /> 
   </section>
   </Container>
 )
